927df0e03f956c514ff206b0ef54be331a41b89c
iamminho <minho100227@kakao.com>
2023-06-02 23:19:58 +0900

    useEffect

--- src/App.js
+++ src/App.js
@@ -1,23 +1,34 @@
-import logo from './logo.svg';
-import './App.css';
+// import Button from "./Button";
+// import style from "./App.module.css";
+import { useState, useEffect } from "react";
 
 function App() {
+  const [counter, setCounter] = useState(0);
+  const [keyword, setKeyword] = useState("");
+
+  const onClick = () => setCounter((prev) => prev + 1);
+  const onChange = (event) => setKeyword(event.target.value);
+
+  console.log("rendering");
+
+  useEffect(() => {
+    console.log("CALL THE API....");
+  }, []);
+
+  useEffect(() => {
+    if (keyword !== "") console.log("SEARCH FOR", keyword);
+  }, [keyword]);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <input
+        value={keyword}
+        type="text"
+        placeholder="Search here ..."
+        onChange={onChange}
+      />
+      <h1>{counter}</h1>
+      <button onClick={onClick}>plus</button>
     </div>
   );
 }
