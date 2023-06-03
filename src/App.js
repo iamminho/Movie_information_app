a265e195e6788f4cd245b036d3c9599a48c6b1bc
iamminho <minho100227@kakao.com>
2023-06-03 14:21:46 +0900

    To Do List Part One

--- src/App.js
+++ src/App.js
@@ -3,32 +3,28 @@
 import { useState, useEffect } from "react";
 
 function App() {
-  const [counter, setCounter] = useState(0);
-  const [keyword, setKeyword] = useState("");
-
-  const onClick = () => setCounter((prev) => prev + 1);
-  const onChange = (event) => setKeyword(event.target.value);
-
-  console.log("rendering");
-
-  useEffect(() => {
-    console.log("CALL THE API....");
-  }, []);
-
-  useEffect(() => {
-    if (keyword !== "") console.log("SEARCH FOR", keyword);
-  }, [keyword]);
-
+  const [toDo, setToDo] = useState("");
+  const [toDos, setToDos] = useState([]);
+  const onChange = (event) => setToDo(event.target.value);
+  const onSubmit = (event) => {
+    event.preventDefault();
+    if (toDo === "") return;
+    setToDos((currentArray) => [toDo, ...currentArray]);
+    setToDo("");
+  };
+  console.log(toDos);
   return (
     <div>
-      <input
-        value={keyword}
-        type="text"
-        placeholder="Search here ..."
-        onChange={onChange}
-      />
-      <h1>{counter}</h1>
-      <button onClick={onClick}>plus</button>
+      <h1>My To Dos ({toDos.length})</h1>
+      <form onSubmit={onSubmit}>
+        <input
+          onChange={onChange}
+          value={toDo}
+          type="text"
+          placeholder="Write your to do"
+        />
+        <button>Add To Do</button>
+      </form>
     </div>
   );
 }
