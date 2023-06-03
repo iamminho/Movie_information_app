5ece1e11ce7195625ca190135b090cb4b5137d69
iamminho <minho100227@kakao.com>
2023-06-03 15:09:18 +0900

    first commit

--- src/App.js
+++ src/App.js
@@ -1,38 +1,5 @@
-// import Button from "./Button";
-// import style from "./App.module.css";
-import { useState, useEffect } from "react";
-
 function App() {
-  const [toDo, setToDo] = useState("");
-  const [toDos, setToDos] = useState([]);
-  const onChange = (event) => setToDo(event.target.value);
-  const onSubmit = (event) => {
-    event.preventDefault();
-    if (toDo === "") return;
-    setToDos((currentArray) => [toDo, ...currentArray]);
-    setToDo("");
-  };
-
-  return (
-    <div>
-      <h1>My To Dos ({toDos.length})</h1>
-      <form onSubmit={onSubmit}>
-        <input
-          onChange={onChange}
-          value={toDo}
-          type="text"
-          placeholder="Write your to do"
-        />
-        <button>Add To Do</button>
-      </form>
-      <hr />
-      <ul>
-        {toDos.map((item, idx) => (
-          <li key={idx}>{item}</li>
-        ))}
-      </ul>
-    </div>
-  );
+  return <div></div>;
 }
 
 export default App;
